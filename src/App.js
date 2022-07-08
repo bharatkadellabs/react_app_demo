1d2734fb2118ae689c5e4fdbdc9fa01e21265f81
bharat <bharat.kumar@kadellabs.com>
2022-07-08 14:55:56 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import Header from './componets/Header';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+
+
+
 
 
 const App = () => {
   return (
-    <Header />
-    )
+    <div>
+      <Router>
+        <Header></Header>
+       
+        <Routes>
+         
+          <Route exact path='/' component={Home} />
+          <Route exact path='/SignUp' component={SignUp} />
+          <Route exact path='/SignIn' component={SignIn} />
+        
+        </Routes>
+      </Router>
+    </div>
+  )
 }
 
 export default App;
